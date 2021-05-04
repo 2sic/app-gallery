af84cb5deba4604e82e0c5619809b70f20ac7202
Roman Opalko <roman.opalko@2sic.com>
2021-05-04 10:38:55 +0200

    reorg js, add lazyload correctly

--- src/ts/fancybox.ts
+++ src/ts/fancybox.ts
@@ -8,13 +8,5 @@ export function fancyboxGallery3() {
           return $(this).find('figcaption').html();
         }
       });
-    }
-  
-    /* Lazy - only if jQuery Animations exists */
-    if (($('.lazy') as any).Lazy) {
-      ($('.lazy') as any).Lazy({
-        effect: 'fadeIn',
-        effectTime: 200,
-      });
-    }
+    } 
   }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,22 @@
 import { fancyboxGallery3 } from './fancybox';
+import { activateLazyLoad } from './lazyload';
 import { monkeyPatchjQueryFade } from './jquery-fade-in';
+
+/** activate all the content-app features */
+function activateGalleryAll() {
+  console.log('activateGalleryAll')
+  fancyboxGallery3();
+  activateLazyLoad();
+}
 
 $(monkeyPatchjQueryFade);
 
-// Add to window, so that each load of the module can call this
-// This is important, because the script may have already been loaded when the 
-// content is gradually being added
-(window as any).appFancyboxGallery3Init = fancyboxGallery3;
+// Add window.appContent.activateAll() 
+// so it can be called from the HTML when content re-initializes dynamically
+const win2GalExt = (window as any);
+const appG = win2GalExt.appGallery = win2GalExt.appGallery || {};
+appG.activateGalleryAll = appG.activateGalleryAll || activateGalleryAll;
+
+// If loaded the first time on a dynamic page, activate automatically
+// Later reloads will need to call the activateAll from the reloaded content
+$(activateGalleryAll);
