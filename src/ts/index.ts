2489290eccf9dd05c796405b495b3553b7b20f37
Thomas Lendi <thomas.lendi@2sic.com>
2021-08-27 12:28:10 +0200

    optimize SCSS and Structure

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,20 @@
 declare let Fancybox: any;
 
+// This is only partial - if you want to see more, see https://fancyapps.com/docs/ui/fancybox/options
+interface FancyboxOptions {
+    groupAll: boolean;
+    Thumbs: {
+      autostart: boolean;
+    }
+    startIndex?: number;
+    preload?: number;
+    infinite?: boolean;
+    autoFocus?: boolean;
+}
+
 (window as any).appGalleryV7 = {
-    initFancybox: function initFancybox(module: any) {
-        Fancybox.bind(`[data-app-gallery-fancybox="gallery-${module.moduleId}"]`, module.options);        
+    initFancybox: function initFancybox({ groupId, options } : { groupId: string, options: FancyboxOptions }) {
+        console.log(groupId)
+        Fancybox.bind(`[data-app-gallery-fancybox="${groupId}"]`, options);
     }
 }
