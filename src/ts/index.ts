9f04ba5d04fe98f4d58b5ce4034b06825d156d0e
Roman Opalko <roman.opalko@2sic.com>
2021-08-27 07:18:27 +0200

    refactoring

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,6 @@
 declare let Fancybox: any;
 
-(window as any).appFb = {
+(window as any).appGalleryV7 = {
     initFancybox: function initFancybox(module: any) {
         Fancybox.bind(`[data-app-gallery-fancybox="gallery-${module.moduleId}"]`, module.options);        
     }
