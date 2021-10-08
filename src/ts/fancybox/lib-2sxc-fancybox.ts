ef3ee4027df47ddedc320156745aebaa5ab45e2e
Maxim Hüsler <maxim.huesler@2sic.com>
2021-10-08 14:23:43 +0200

    fb assets standardized

--- src/ts/fancybox/lib-2sxc-fancybox.ts
+++ src/ts/fancybox/lib-2sxc-fancybox.ts
@@ -5,10 +5,11 @@ import { FancyboxOptions } from './lib-2sxc-fancybox-options';
   This is a shared code used in various 2sxc apps. Make sure that they are in sync, so if you improve it, improve all 2sxc apps which use this. 
   ATM they are:
   - Gallery7
+  - Content
   The master with the newest / best version must always be the Gallery7, the others should then get a fresh copy.
   Because this is shared, all parameters like DOM-IDs etc. must be provided in the Init-call that it can work across apps
 */ 
 
 export function initFancybox({ domId, options } : { domId: string, options: FancyboxOptions }) {
-    Fancybox.bind(`.${domId}`, options);
+  Fancybox.bind(`.${domId}`, options);
 }
